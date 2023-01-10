ab8d128a373d5560bb2c3ecb585cce63e7b3e379
sallanez <saul.alejandro19@gmail.com>
2023-01-10 11:55:11 -0600

    navbar path error on App.jsx 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { ReactDOM } from "react";
 import Navbar from "./components/Navbar.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Explore from "./pages/Explore.jsx";
