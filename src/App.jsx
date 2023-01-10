52189fc5add4e1ec2b81a7dfa38c29c750189c8d
sallanez <saul.alejandro19@gmail.com>
2023-01-10 16:13:41 -0600

    this bug is hard

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
           <Route path="/contact/:landlordId" element={<ContactLandLord />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
-        {Navbar && <Navbar />}
+        <Navbar />
       </Router>
       <ToastContainer />
     </>
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
